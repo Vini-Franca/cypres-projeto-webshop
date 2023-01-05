ecf7a5f8677f60c5ac9c7b27e9adc0fcda37669b
Vini_Franca <vinicius_santana_rf@hotmail.com>
2023-01-04 23:11:53 -0300

    addPC

--- cypress/e2e/shoppingCart.cy.js
+++ cypress/e2e/shoppingCart.cy.js
@@ -4,11 +4,31 @@ import ShoppingCartPage from "../pages/ShoppingCartPage";
 describe('Adicionar produtos ao carrinho', function () {
     it('Adicionar um livro ao carrinho', function () {
         var client_data = SignupFactory.client_data();
-       const expectedMessage = 'The product has been added to your ';  
+        const expectedMessage = 'The product has been added to your ';  
 
         ShoppingCartPage.go();
         ShoppingCartPage.login(client_data);
         ShoppingCartPage.addBook();
         ShoppingCartPage.addToCartValidation(expectedMessage);
+    });
+
+    it('Adicionar um PC ao carrinho', function () {
+        var client_data = SignupFactory.client_data();
+        const expectedMessage = 'The product has been added to your ';  
+
+        ShoppingCartPage.go();
+        ShoppingCartPage.login(client_data);
+        ShoppingCartPage.addPC();
+        ShoppingCartPage.addToCartValidation(expectedMessage);
+    })
+
+    it('Limpar carrinho', function () {
+        var client_data = SignupFactory.client_data();
+        const expectedMessage = 'Your Shopping Cart is empty!'  
+
+        ShoppingCartPage.go();
+        ShoppingCartPage.login(client_data);
+        ShoppingCartPage.clearCart();
+        ShoppingCartPage.clearCartValidation(expectedMessage);
     })
 })
--- cypress/pages/ShoppingCartPage.js
+++ cypress/pages/ShoppingCartPage.js
@@ -3,6 +3,13 @@ class ShoppingCartPage {
       cy.visit("https://demowebshop.tricentis.com/");
       cy.title("Demo Web Shop");
     }
+    
+    login(client_data) {
+      cy.get("a[class='ico-login']").click();
+      cy.get('input[id="Email"]').type(client_data.email);
+      cy.get('input[id="Password"]').type(client_data.password);
+      cy.get("input[class='button-1 login-button']").click();
+    }
   
     addBook() {
         cy.get('a[href="/books"]').first().click();
@@ -12,17 +19,32 @@ class ShoppingCartPage {
         cy.get('input[id="add-to-cart-button-45"]').click();
     }
 
-  
+    addPC() {
+      cy.get('a[href="/computers"]').first().click();
+      cy.get('h2[class="title"]').first().click();
+      cy.get('a[href="/build-your-own-expensive-computer-2"]').last().click();
+      cy.get('input[id="product_attribute_74_5_26_82"]').click();
+      cy.get('input[id="product_attribute_74_6_27_85"]').click();
+      cy.get('input[id="product_attribute_74_3_28_87"]').click();
+      cy.get('input[id="product_attribute_74_8_29_89"]').click();
+      cy.get('input[id="add-to-cart-button-74"]').click();
+  }
+
     addToCartValidation(expectedMessage) {
       cy.contains(expectedMessage);
     }
 
-    login(client_data) {
-        cy.get("a[class='ico-login']").click();
-        cy.get('input[id="Email"]').type(client_data.email);
-        cy.get('input[id="Password"]').type(client_data.password);
-        cy.get("input[class='button-1 login-button']").click();
-      }
+    clearCart() {
+      cy.get('span[class="cart-label"]').first().click();
+      cy.get('input[name="removefromcart"]')
+      .as('checkboxes')
+      .check();
+      cy.get('input[name="updatecart"]').click()
+    }
+
+    clearCartValidation(expectedMessage) {
+      cy.contains(expectedMessage);
+    }
   }
   
   export default new ShoppingCartPage();
